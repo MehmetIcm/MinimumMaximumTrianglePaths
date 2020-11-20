fe79aa3faf07e1a4a2499a24a9feb2e2c4a7d294
Mehmet ICME <44260512+MehmetIcm@users.noreply.github.com>
2020-11-20 15:02:00 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumMaximumTrianglePaths
+{
+    class Program
+    {
+        //We want to short a kind of matrix but it's not a square, it's a triangle. So if we make generic list with lists we can make a triangle
+        public static void Main()
+        {
+            List<List<int>> A = new List<List<int>>();
+            A.Add(new List<int> { 7 });
+            A.Add(new List<int> { 6, 3 });
+            A.Add(new List<int> { 3, 8, 5 });
+            A.Add(new List<int> { 11, 2, 10, 9 });
+            Console.WriteLine(minSumPath(ref A));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MinimumMaximumTrianglePaths
+ An application made to find the minimum path from triangular numbers
